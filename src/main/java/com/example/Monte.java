188acee576bdd473ce5b9f761c7b65389026fa45
leiti2013 <leiticontato@hotmail.com>
2024-02-09 10:31:21 -0300

    5 Classes e métodos feitos

---
+++ src/main/java/com/example/Monte.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monte {
+    private List<Carta> cartas = new ArrayList<>();
+
+    public Monte(){
+        for(Naipe naipe: Naipe.values())
+            for(int i = 1; i<=13; i++){
+                cartas.add(new Carta(i, naipe));
+            }
+    }
+}
